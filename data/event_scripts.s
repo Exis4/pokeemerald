aa3dc3220bbe4ef6496151158ad38dcd87e2b00f
Exis4 <25187645+Exis4@users.noreply.github.com>
2025-12-27 12:38:28 -0300

    Rival starter egg

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -42,6 +42,7 @@
 #include "constants/party_menu.h"
 #include "constants/pokemon.h"
 #include "constants/roulette.h"
+#include "constants/region_map_sections.h"
 #include "constants/script_menu.h"
 #include "constants/secret_bases.h"
 #include "constants/songs.h"
